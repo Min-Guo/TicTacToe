fb07e80298dd3efbf301a87469a1f020a66467c1
Min Guo <xmguomin@gmail.com>
2014-05-13 23:43:36 -0400

    Add all the files

---
+++ script.js
@@ -0,0 +1,65 @@
+var currentPlayer= 0;
+
+var boardStatus = [0,0,0,0,0,0,0,0,0];
+
+function player1choice(){
+  currentPlayer =1;
+}
+function player2choice(){
+  currentPlayer =2;
+}
+
+$("td").click(function(){
+  boardStatus[$(this).data("id")] = currentPlayer;
+  $(this).html(currentPlayer);
+ console.log($(this).data("id"));
+ console.log(boardStatus);
+ //function checkStatus(){
+  if (winGame() === true){
+    alert("Player" + currentPlayer + "  Win!")
+  console.log(winGame());
+  }
+})
+
+function winGame(){
+  if ( boardStatus[0] === currentPlayer
+    && boardStatus[3] === currentPlayer
+    && boardStatus[6] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[0] === currentPlayer
+    && boardStatus[1] === currentPlayer
+    && boardStatus[2] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[3] === currentPlayer
+    && boardStatus[4] === currentPlayer
+    && boardStatus[5] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[6] === currentPlayer
+    && boardStatus[7] === currentPlayer
+    && boardStatus[8] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[2] === currentPlayer
+    && boardStatus[5] === currentPlayer
+    && boardStatus[8] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[0] === currentPlayer
+    && boardStatus[4] === currentPlayer
+    && boardStatus[8] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[2] === currentPlayer
+    && boardStatus[4] === currentPlayer
+    && boardStatus[6] === currentPlayer){
+    return true;
+  }
+  if ( boardStatus[1] === currentPlayer
+    && boardStatus[4] === currentPlayer
+    && boardStatus[7] === currentPlayer){
+    return true;
+  }
+}
