80b92c1d67490ad82e41d05fda453d65371cc609
Min Guo <xmguomin@gmail.com>
2014-05-20 14:02:21 -0400

    Make computer win when there have already been two 'X' on the same line.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var player =1;
 
 var computer = 2;
 
+var winId;
+
+
 $("td").click(function(){
   if (boardStatus[$(this).data("id")] === 0){
     var clickId = $(this).data("id");
@@ -15,11 +18,19 @@ function main(clickId){
   if (handlePlay(clickId, player) === true){
     resetTable();
   } else{
-    var computerId = randomId();
-    if (handlePlay(computerId, computer) === true){
+      computerId();
+    if (handlePlay(computerId(), computer) === true){
       resetTable();
+      }
     }
-  }
+}
+
+function computerId(){
+  if (selectWin() !== false){
+    return selectWin();
+  } else {
+    return randomId();
+  } 
 }
 
 function handlePlay(id, currentPlayer){
@@ -117,4 +128,59 @@ function resetTable(){
   $("td").html("");
 }
 
+function convertArr(row, column){
+  return boardStatus[3 * row + column];
+}
 
+function rowWin(){
+  for (var row = 0; row < 3; row++){
+    console.log(row);
+    if (convertArr(row, 0) === 2 && convertArr(row, 1) === 2 && convertArr(row, 2) === 0){
+        return winId = row * 3 + 2;
+    } else if (convertArr(row, 0) === 2 && convertArr(row, 2) === 2 && convertArr(row, 1) === 0){
+        return winId = row * 3 + 1;
+    } else if (convertArr(row, 2) === 2 && convertArr(row, 1) === 2 && convertArr(row, 0) === 0){
+        return winId = row * 3 + 0;
+    } 
+  } 
+  return false;
+}
+
+function columnWin(){
+  for (var column = 0; column < 3; column++){
+    console.log(column);
+    if (convertArr(0, column) === 2 && convertArr(1, column) === 2 && convertArr(2, column) === 0){
+        return winId = 2 * 3 + column;
+    } else if (convertArr(0, column) === 2 && convertArr(2, column) === 2 && convertArr(1, column) === 0){
+        return winId = 1 * 3 + column;
+    } else if (convertArr(1, column) === 2 && convertArr(2, column) === 2 && convertArr(0, column) === 0){
+        return winId = 0 * 3 + column;
+    } 
+  } return false;
+}
+
+function digonalWin(){
+  if (convertArr(0, 0) === 2 && convertArr(1, 1) === 2 && convertArr(2, 2) === 0){
+      return winId = 8;
+  } else if (convertArr(1, 1) === 2 && convertArr(2, 2) === 2 && convertArr(0, 0) === 0){
+      return winId = 0;
+  } else if (convertArr(0, 0) === 2 && convertArr(2, 2) === 2 && convertArr(1, 1) === 0){
+      return winId = 4;
+  } else if (convertArr(0, 2) === 2 && convertArr(1, 1) === 2 && convertArr(2, 0) === 0){
+      return winId = 6;
+  } else if (convertArr(0, 2) === 2 && convertArr(2, 0) === 2 && convertArr(1, 1) === 0){
+      return winId = 4;
+  } else if (convertArr(1, 1) === 2 && convertArr(2, 0) === 2 && convertArr(0, 2) === 0){
+      return winId = 2;
+  } else{
+      return false;
+  }
+}
+
+function selectWin(){
+  if (rowWin() === false && columnWin() === false && digonalWin() === false){
+    return false;
+  } else{
+      return winId;
+  }
+}
